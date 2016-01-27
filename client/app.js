924d41a17120b7fde249df3b7e4f8fde6c433850
Irene K <linqwn@gmail.com>
2016-01-27 12:43:52 -0800

    Experimenting with different css

--- client/app.js
+++ client/app.js
@@ -31,10 +31,6 @@ angular.module('app', [
         'footer': {
           templateUrl: 'app/main/main.footer.html',
           controller: 'MainController'
-        },
-        'signup': {
-          templateUrl: 'app/auth/signin.html',
-          controller: 'AuthController'
         }
       }
     })
